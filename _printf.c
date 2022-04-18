2a40dd41c3c81633f95d4560c465df41433e6527
idriss <djiofackidriss@gmail.com>
2022-04-19 02:29:46 +0800

    Update _printf file

--- _printf.c
+++ _printf.c
@@ -1,82 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+int _printf(char *format, ...)
 {
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	int written = 0, (*print_fn)(char *, va_list);
+	char specifier[3];
+	va_list args;
 
 	if (format == NULL)
 		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	specifier[2] = '\0';
+	va_start(args, format);
+	_putchar(-1);
+	while (format[0])
 	{
-		for (; format[i] != '%' && format[i]; i++)
+		if (format[0] == '%')
 		{
-			_putchar(format[i]);
-			count++;
+			print_fn = get_print_fn(format);
+			if (print_fn)
+			{
+				specifier[0] = '%';
+				specifier[1] = format[1];
+				written += print_fn(specifier, args);
+			}
+			else if (format[1] != '\0')
+			{
+				written += _putchar('%');
+				written += _putchar(format[1]);
+			}
+			else
+			{
+				written += _putchar('%');
+				break;
+			}
+			format += 2;
 		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
 		else
-			i++;
+		{
+			written += _putchar(format[0]);
+			format++;
+		}
 	}
-	va_end(valist);
-	return (count);
+	_putchar(-2);
+	return (written);
 }
